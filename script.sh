a4d6e821628d58b9419ed13e35024ea654a5462a
hubert <guciokobos@gmail.com>
2026-05-04 13:09:56 +0200

    More repos,working and more readable

--- script.sh
+++ script.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 #checking if user is owner of the directory or is root
 if [ "$(stat -c '%U' "$1")" == "$USER" ] || [ "$USER" == "root" ]; then
     echo "User is the owner of the directory or is root. Cloning repositories..."
@@ -6,6 +7,9 @@ if [ "$(stat -c '%U' "$1")" == "$USER" ] || [ "$USER" == "root" ]; then
     git clone -q https://github.com/Flangvik/SharpCollection "$1/SharpCollection"
     git clone -q https://github.com/danielmiessler/seclists "$1/seclists"
     git clone -q https://github.com/urbanadventurer/username-anarchy "$1/username-anarchy"
-    # Add more repositories as needed
-    echo "done"
+    git clone -q https://github.com/RedSiege/EyeWitness "$1/eyewitness"
+    git clone -q https://github.com/samratashok/nishang "$1/nishang"
+    git clone -q https://github.com/samratashok/nishang "$1/nishang"
+    git clone -q https://github.com/arthaud/git-dumper "$1/git-dumper"
+    # Add more repositories in next commits
 fi
