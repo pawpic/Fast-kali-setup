95d0633130b3e0dcb838f175d2769aab19da2255
hubert <guciokobos@gmail.com>
2026-05-18 17:26:50 +0200

    Modify directory ownership check and add dependencies
    Adding information about cloning and dev version of kerbrute

--- script.sh
+++ script.sh
@@ -1,17 +1,25 @@
 #!/bin/bash
 
-#checking if user is owner of the directory or is root
-if [ "$(stat -c '%U' "$1")" == "$USER" ] || [ "$USER" == "root" ]; then
-    echo "User is the owner of the directory or is root. Cloning repositories..."
+#checking if user is owner of the directory and is root
+if [ "$(stat -c '%U' "$1")" == "$USER" ] && [ "$USER" == "root" ]; then
+    echo "User is root. Cloning repositories..."
     # Clone the repositories here
+    echo "Sharp collections"
     git clone -q https://github.com/Flangvik/SharpCollection "$1/SharpCollection"
+    echo "Seclists"
     git clone -q https://github.com/danielmiessler/seclists "$1/seclists"
+    echo "Username anarchy"
     git clone -q https://github.com/urbanadventurer/username-anarchy "$1/username-anarchy"
+    echo "Eyewitness"
     git clone -q https://github.com/RedSiege/EyeWitness "$1/eyewitness"
+    echo "Nishang"
     git clone -q https://github.com/samratashok/nishang "$1/nishang"
+    echo "Git-Dumper"
     git clone -q https://github.com/arthaud/git-dumper "$1/git-dumper"
+    echo "Kerbrute"
     git clone -q https://github.com/ropnop/kerbrute "$1/kerbrute"
-    # Installing dev version of kerbrute
+    echo "Installing go-lang and dev version of kerbrute. Also krb5-user"
+    apt install golang krb5-user
     cd "$1/kerbrute"
     make linux
 fi
