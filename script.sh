a28fb0525552047eef15bc6cfb5e09da2a5a8957
hubert <guciokobos@gmail.com>
2026-05-18 17:29:16 +0200

    Update echo statements for clarity in script.sh

--- script.sh
+++ script.sh
@@ -4,22 +4,22 @@
 if [ "$(stat -c '%U' "$1")" == "$USER" ] && [ "$USER" == "root" ]; then
     echo "User is root. Cloning repositories..."
     # Clone the repositories here
-    echo "Sharp collections"
+    echo "Cloning Sharp collections"
     git clone -q https://github.com/Flangvik/SharpCollection "$1/SharpCollection"
-    echo "Seclists"
+    echo "Cloning Seclists"
     git clone -q https://github.com/danielmiessler/seclists "$1/seclists"
-    echo "Username anarchy"
+    echo "Cloning Username anarchy"
     git clone -q https://github.com/urbanadventurer/username-anarchy "$1/username-anarchy"
-    echo "Eyewitness"
+    echo "Cloning Eyewitness"
     git clone -q https://github.com/RedSiege/EyeWitness "$1/eyewitness"
-    echo "Nishang"
+    echo "Cloning Nishang"
     git clone -q https://github.com/samratashok/nishang "$1/nishang"
-    echo "Git-Dumper"
+    echo "Cloning Git-Dumper"
     git clone -q https://github.com/arthaud/git-dumper "$1/git-dumper"
-    echo "Kerbrute"
+    echo "Cloning Kerbrute"
     git clone -q https://github.com/ropnop/kerbrute "$1/kerbrute"
     echo "Installing go-lang and dev version of kerbrute. Also krb5-user"
-    apt install golang krb5-user
+    apt install golang krb5-user 2>/dev/null
     cd "$1/kerbrute"
     make linux
 fi
